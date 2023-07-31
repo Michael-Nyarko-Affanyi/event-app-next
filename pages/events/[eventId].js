5281bd607884fa823a7257b99182d3e62e92bc82
Michael Nyarko Affanyi <cwajodepay@gmail.com>
2023-07-31 16:45:11 +0000

    feat: added single event page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,34 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSumary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 const SingleEvent = () => {
-    return (
-        <div>Single Event</div>
-    )
+  const {
+    query: { eventId: id },
+  } = useRouter();
+
+  const singleEvent = getEventById(id);
+  console.log(singleEvent);
+
+  if (!singleEvent) {
+    return <p>No event found!</p>;
+  }
+  return (
+    <>
+      <EventSumary title={singleEvent.title} />
+      <EventLogistics
+        date={singleEvent.date}
+        address={singleEvent.location}
+        image={singleEvent.image}
+        imageAlt={singleEvent.title}
+      />
+      <EventContent>
+        <p>{singleEvent.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default SingleEvent;
